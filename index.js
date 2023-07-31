83ab050cc8db037b8865bb7d5697830d5322e397
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-07-31 08:21:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const accesKey = "motxySnZDlq-7QF6QUD6QT1LTAQ8M1fthUsvt_ldzyI"
+const form = document.querySelector(".form");
+const input = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+
+let inputData = ""
+let page = 1;
+
+async function searchImages(){
+    inputData = input.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results = data.results
+
+
+    if ( page === 1){ 
+        searchResults.ineerHTML = ""
+     }
+ results.map((result) =>{
+    const imageWrapper = document.createElement('div')
+    imageWrapper.classList.add("search-result")
+    const image = document.createElement('img')
+    image.src = result.urls.small
+    image.alt = result.alt_description
+    const imageLink = document.createElement('a')
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageLink.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+    
+
+
+
+ });
+
+ page ++;
+ if(page > 1){
+    showMore.style.display = "block"
+ }
+}
+
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    page = 1;
+    searchImages();
+});
+
+
+showMore.addEventListener("click", ()=>{
+    searchImages();
+});
